d6e6de4c7b03b6c17da11a055e27ada5118b657a
root <yuantang.lin@fit2cloud.com>
2018-10-10 19:11:46 +0800

    20181010

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World linyt defaultphptest222</p>'; ?> 
+ <?php echo '<p>Hello World linyt 20181010new</p>'; ?> 
  </body>
 </html>
